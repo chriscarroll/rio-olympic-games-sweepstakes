7d480d3110606dbfe84ca44298d30bb2d674501f
Chris Carroll <chris.carroll1@gmail.com>
2016-08-12 01:11:52 +1000

    write generated factors to file

--- public/js/factorGenerator.js
+++ public/js/factorGenerator.js
@@ -7,17 +7,25 @@ var data = '';
 var GOLD_POINTS = 10;
 var SILVER_POINTS = 5;
 var BRONZE_POINTS = 3;
+var MAX_POINTS_FACTOR = 20;
 
 readableStream.on('data', function(chunk) {
   data += chunk;
 });
 
 readableStream.on('end', function() {
-  var jsonData = tsvToJson(data);
-  console.log('*** json data created ***');
-  console.log(JSON.stringify(jsonData));
-  var factorData = generateFactors(jsonData);
-  console.log('*** factor data created ***');
+  var countryData = tsvToJson(data);
+  console.log('*** country json data created ***');
+  console.log(JSON.stringify(countryData));
+  var factorData = generateFactors(countryData);
+  console.log('*** factor json data created ***');
+  console.log(JSON.stringify(factorData));
+  fs.writeFile(path.join(__dirname, '../resources/generated/country_factors_2016.json'), JSON.stringify(factorData), function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log('The file was saved!');
+  });
 });
 
 // Function to convert tsv file with column headers to json
@@ -35,15 +43,25 @@ function tsvToJson(tsvData) {
     }
     result.push(obj);
   }
-  //return JSON.stringify(result);
   return result;
 }
 
-// Function to generate factors for all countries at previous olympics
+// Function to generate factors for all countries based on 2012 olympic games results
 function generateFactors(jsonData) {
+  var result = [];
   for (var i = 0, length = jsonData.length; i < length; i++) {
     var totalPoints = jsonData[i].gold * GOLD_POINTS + jsonData[i].silver * SILVER_POINTS + jsonData[i].bronze * BRONZE_POINTS;
     var factor = (100 / totalPoints).toFixed(5);
-    console.log(jsonData[i].country + ' ' + factor);
+
+    // a country's factor can't exceed 20 points
+    factor = factor > MAX_POINTS_FACTOR ? MAX_POINTS_FACTOR : factor;
+
+    var obj = {
+      country: jsonData[i].country,
+      code: jsonData[i].code,
+      factor: factor
+    };
+    result.push(obj);
   }
+  return result;
 }
