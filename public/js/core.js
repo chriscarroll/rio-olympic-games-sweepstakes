a4546711318ec72cbad0e7c58906726f03856aec
Chris Carroll <chris.carroll1@gmail.com>
2016-08-16 02:00:09 +1000

    update api called for performance reasons and apply factors

--- public/js/core.js
+++ public/js/core.js
@@ -8,66 +8,34 @@ app.controller('mainController', function($scope, $http, $q) {
 
   $scope.results = [];
 
-  $http.get('/teams').then(function(response) {
-    var deferred = $q.defer();
-    var urlCalls = [];
-    for (var i = 0, teamsLength = response.data.length; i < teamsLength; i++) {
-      var obj = {
-        name: response.data[i].name,
-        countries: response.data[i].countries
-      };
-      $scope.results.push(obj);
-      for (var j = 0, countriesLength = response.data[i].countries.length; j < countriesLength; j++) {
-        // TODO: Factor calculation/substitution
-        var promise = $http.get('/medals/' + response.data[i].countries[j]).then(function(response) {
-          return {
-            countryId: response.data.id,
-            gold: response.data.gold_count,
-            silver: response.data.silver_count,
-            bronze: response.data.bronze_count,
-            points: response.data.gold_count * GOLD_POINTS + response.data.silver_count * SILVER_POINTS + response.data.bronze_count * BRONZE_POINTS,
-            factor: 1
-          };
-        }, function(err) {
-          // Country has won no medals so default everything to 0
-          if (err.data && err.data.error_msg) {
-            console.log('Error: ' + err.data.error_msg);
-          } else {
-            console.dir(err);
-          }
-          // TODO: This isn't pretty but necessary for now to work with the multiple xhr requests needed for this data
-          return {
-            countryId: err.config.url.replace('/medals/', ''),
-            gold: 0,
-            silver: 0,
-            bronze: 0,
-            points: 0,
-            factor: 0
-          };
+  var teams = $http.get('/teams');
+  var medals = $http.get("/medals");
+  var factors = $http.get("../resources/generated/country_factors_2016.json");
+
+  $q.all([teams, medals, factors]).then(function(results) {
+    for (var i = 0, teamsLength = results[0].data.length; i < teamsLength; i++) {
+      var name = results[0].data[i].name;
+      var countries = results[0].data[i].countries;
+      var points = 0;
+      for (var j = 0, countriesLength = countries.length; j < countriesLength; j++) {
+        var factor = 0;
+          // TODO: replace with lodash find, ie: _.find(results, {countryId: value});
+          angular.forEach(results[1].data, function(valueB, index) {
+            if (valueB.id === countries[j]) {
+              angular.forEach(results[2].data, function(valueC, index) {
+                if (valueC.countryId === countries[j]) {
+                  factor = valueC.factor;
+                }
+              });
+              points += (valueB.gold_count * GOLD_POINTS + valueB.silver_count * SILVER_POINTS + valueB.bronze_count * BRONZE_POINTS) * factor;
+            }
         });
-        urlCalls.push(promise);
       }
-    }
-    $q.all(urlCalls).then(function(results) {
-        //deferred.resolve(results);
-        for (var i= 0; i < $scope.results.length; i++) {
-          var points = 0;
-          angular.forEach($scope.results[i].countries, function(valueA, index) {
-            // TODO: replace with lodash find, ie: _.find(results, {countryId: value});
-            angular.forEach(results, function(valueB, index) {
-              if (valueB.countryId === valueA) {
-                points += valueB.points;
-              }
-            });
-          });
-          $scope.results[i].points = points;
-        }
-      },
-      function(errors) {
-        deferred.reject(errors);
-      },
-      function(updates) {
-        deferred.update(updates);
+      $scope.results.push({
+        name: name,
+        countries: countries,
+        points: points.toFixed(2)/1
       });
+    }
   });
 });
