9abb6ab22b76663ba0ddd6d6204d40a0435f4fae
Chris Carroll <chris.carroll1@gmail.com>
2016-08-16 23:48:30 +1000

    add favicon

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var teams = require('./routes/teams');
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
